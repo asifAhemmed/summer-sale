af87fd18459021ad4c8b0a6cb6e879e992b986dc
asifrahi9 <asifahemmed0@gmail.com>
2024-02-22 19:39:41 +0600

    bug fixed

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,38 +1,67 @@
-function getAmount(productId){
-    const product = document.getElementById(productId);
-    const productAmountString = product.innerText;
-    const amount = parseFloat(productAmountString);
+function getAmount(productId) {
+  const product = document.getElementById(productId);
+  const productAmountString = product.innerText;
+  const amount = parseFloat(productAmountString);
 
-    return amount;
+  return amount;
 }
 
-function setTotalPrice(amount){
-    const totalPriceValue = document.getElementById("total-price");
-    const totalPriceValueString = totalPriceValue.innerText;
-    const totalPrice = parseFloat(totalPriceValueString);
+function setTotalPrice(amount) {
+  const totalPriceValue = document.getElementById("total-price");
+  const totalPriceValueString = totalPriceValue.innerText;
+  const totalPrice = parseFloat(totalPriceValueString);
 
-    totalPriceValue.innerText = totalPrice + amount ;
+  totalPriceValue.innerText = totalPrice + amount;
 }
 
-function addToCart(productId,product){
-    const amount = getAmount(productId);
-    setTotalPrice(amount);
-    setProductNameToCart(product);
+function addToCart(productId, product) {
+  const amount = getAmount(productId);
+  setTotalPrice(amount);
+  setProductNameToCart(product);
+  getTotalPrice();
 }
 
-function getProductName(product){
-    const productNameField = document.getElementById(product);
-    const productName = productNameField.innerText;
+function getProductName(product) {
+  const productNameField = document.getElementById(product);
+  const productName = productNameField.innerText;
 
-    return productName;
+  return productName;
 }
 
-function setProductNameToCart(product){
-    const productNameContainer = document.getElementById('productName-container');
-    const paragraph = document.createElement('p');
-    const productName = getProductName(product);
-    paragraph.classList.add('font-medium');
-    paragraph.innerText = productName;
+function setProductNameToCart(product) {
+  const productNameContainer = document.getElementById("productName-container");
+  const paragraph = document.createElement("p");
+  const productName = getProductName(product);
+  paragraph.classList.add("font-medium");
+  paragraph.innerText = productName;
 
-    productNameContainer.appendChild(paragraph);
+  productNameContainer.appendChild(paragraph);
 }
+function getDiscount() {
+  const totalPrice = getAmount("total-price");
+  const discountField = document.getElementById("discount");
+  const discount = totalPrice * 0.2;
+  discountField.innerText = discount;
+}
+
+function getTotalPrice() {
+    const totalAmountArea = document.getElementById("total-amount");
+    const totalPrice = getAmount("total-price");
+    const totalDiscount = getAmount("discount");
+    const totalAmount = totalPrice - totalDiscount ;
+    totalAmountArea.innerText = totalAmount;
+
+}
+
+document.getElementById("coupon-field").addEventListener("keyup", function () {
+  const couponField = document.getElementById("coupon-field");
+  const buttonApply = document.getElementById("btn-apply");
+  if (couponField.value === "SELL200") {
+    buttonApply.removeAttribute("disabled", false);
+  }
+});
+
+document.getElementById("btn-apply").addEventListener("click", function () {
+    getDiscount();
+    getTotalPrice();
+});
